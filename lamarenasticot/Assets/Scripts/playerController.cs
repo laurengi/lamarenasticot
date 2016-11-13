37b78963113351c1563945f3286ecb96dd12bee4
ravingpanda <pandelganger@gmail.com>
2016-11-13 19:46:20 +0100

    missile management, with collision. Not over yet

--- lamarenasticot/Assets/Scripts/playerController.cs
+++ lamarenasticot/Assets/Scripts/playerController.cs
@@ -143,7 +143,6 @@ public class playerController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         // TODO : store gm in private member and fill it in Start()
-        Debug.Log("OnTriggerEnter");
         gameManager gm = FindObjectOfType<gameManager>();
         if (other.gameObject.CompareTag("Apple"))
         {
