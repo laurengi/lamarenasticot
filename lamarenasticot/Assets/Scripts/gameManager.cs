6ab816ea7753647dcc1e67d5d94e0c357d17a2f5
totk <lucquinv@gmail.com>
2016-11-13 19:58:38 +0100

    player UI

--- lamarenasticot/Assets/Scripts/gameManager.cs
+++ lamarenasticot/Assets/Scripts/gameManager.cs
@@ -11,9 +11,11 @@ public class gameManager : MonoBehaviour
     public GameObject background;
     public GameObject playableArea;
     public GameObject wallModel;
+    public GameObject playerUIModel;
 
     private GameObject[] walls;
     private GameObject[] players;
+    private GameObject[] playerUIs;
     private GameObject[] apples;
 
     public int maxNbOfWalls = 8;
@@ -225,8 +227,8 @@ public class gameManager : MonoBehaviour
             }
             if (!collisionFound)
             {
-                GameObject newApple = (GameObject)Instantiate(playerModel, newPosition, Quaternion.identity);
-                return newApple;
+                GameObject newPlayer = (GameObject)Instantiate(playerModel, newPosition, Quaternion.identity);
+                return newPlayer;
             }
         }
         return null;
@@ -240,6 +242,9 @@ public class gameManager : MonoBehaviour
                 continue;
             players[i] = SpawnRandomPlayer();
             players[i].GetComponent<playerController>().playerId = i;
+            GameObject newPlayerUI = (GameObject)Instantiate(playerUIModel, new Vector3(0,0,0), Quaternion.identity);
+            newPlayerUI.GetComponent<playerUI>().init(i);
+            playerUIs[i] = newPlayerUI;
         }
     }
 
@@ -248,6 +253,7 @@ public class gameManager : MonoBehaviour
         walls = new GameObject[maxNbOfWalls];
         players = new GameObject[maxNbOfPlayers];
         apples = new GameObject[maxNbOfApples];
+        playerUIs = new GameObject[maxNbOfPlayers];
         playableAreaMin = playableArea.transform.TransformPoint(playableArea.GetComponent<SpriteRenderer>().sprite.bounds.min);
         playableAreaMax = playableArea.transform.TransformPoint(playableArea.GetComponent<SpriteRenderer>().sprite.bounds.max);
         SpawnWalls();
@@ -269,6 +275,7 @@ public class gameManager : MonoBehaviour
                 {
                     Destroy(i_collectedObject);
                     apples[i] = null;
+                    playerUIs[i_playerId].GetComponent<playerUI>().gainApple();
                     break;
                 }
             }
--- lamarenasticot/Assets/Scripts/playerUI.cs
+++ lamarenasticot/Assets/Scripts/playerUI.cs
@@ -7,22 +7,48 @@ public class playerUI : MonoBehaviour {
 	public GameObject apple1;
 	public GameObject apple2;
 	public GameObject apple3;
-
-	private int nApples = 3;
+	private int m_playerNumber;
+	private int m_nApples = 0;
+	private Camera m_camera;
 	// Use this for initialization
+
 	void Start () {
-		
+		m_camera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float height = Camera.main.orthographicSize;
+        float width = height * Camera.main.aspect;
+        Debug.Log("height : " + height);
+        Debug.Log("width : " + width);
+        switch(m_playerNumber) {
+        	case 0:
+        		transform.position = new Vector3 (-width + 3, height - 2, 0.0f);
+        		break;
+        	case 1:
+        		transform.position = new Vector3 (width - 4, height - 2, 0.0f);
+        		break;
+        	case 2:
+        		transform.position = new Vector3 (-width + 3, -height + 2.5f, 0.0f);
+        		break;
+        	case 3:
+        		transform.position = new Vector3 (width - 4, -height + 2.5f, 0.0f);
+        		break;
+        	default:
+        		break;
+        }
+	}
+
+	public void init(int playerNumber) {
+		Debug.Log("init");
+		m_playerNumber = playerNumber;
 	}
 
 	public void loseApple() {
-		if (nApples > 0) {
-			nApples--;
-			switch(nApples) {
+		if (m_nApples > 0) {
+			m_nApples--;
+			switch(m_nApples) {
 				case 0 :
 					apple1.SetActive(false);
 					break;
@@ -39,9 +65,10 @@ public class playerUI : MonoBehaviour {
 	}
 
 	public void gainApple() {
-		if (nApples < 3) {
-			nApples++;
-			switch(nApples) {
+		Debug.Log("MOAR APPLE !");
+		if (m_nApples < 3) {
+			m_nApples++;
+			switch(m_nApples) {
 				case 1 :
 					apple1.SetActive(true);
 					break;
